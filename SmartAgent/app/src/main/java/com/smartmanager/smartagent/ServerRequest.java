1c51fdfab7d729895d9a18b0f48377d4873242f1
nichabalie <nichabalie@poste.isima.fr>
2017-06-14 17:15:52 -0400

    Add communication android to android and the GET request from URL.

---
+++ SmartAgent/app/src/main/java/com/smartmanager/smartagent/ServerRequest.java
@@ -0,0 +1,62 @@
+package ca.uqac.socketsapp;
+
+import android.os.AsyncTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Nicolas on 14/06/2017.
+ */
+
+public class ServerRequest extends AsyncTask<String, Void, JSONObject> {
+
+
+
+    public  JSONObject doInBackground(String... urlString) {
+
+        try {
+            HttpURLConnection urlConnection = null;
+
+            URL url = new URL(urlString[0]);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+
+            urlConnection.setDoOutput(true);
+
+            urlConnection.connect();
+
+            BufferedReader br=new BufferedReader(new InputStreamReader(url.openStream()));
+
+            char[] buffer = new char[1024];
+
+            String jsonString = new String();
+
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line+"\n");
+            }
+            br.close();
+
+            jsonString = sb.toString();
+
+            System.out.println("JSON: " + jsonString);
+
+            return new JSONObject(jsonString);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
