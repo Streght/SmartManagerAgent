cdcbd1b7e3ef55e4771787b3a35e371a629da062
Max19475 <max_arbelot@hotmail.fr>
2017-06-20 12:45:02 -0400

    Legere modifications sans impact réelles. Signed-off-by: Max19475 <max_arbelot@hotmail.fr>

--- SmartManagerAgent/app/src/main/java/com/smartmanageragent/exteriorCommunication/CommAppImpl.java
+++ SmartManagerAgent/app/src/main/java/com/smartmanageragent/exteriorCommunication/CommAppImpl.java
@@ -15,11 +15,11 @@ public class CommAppImpl implements InterfaceCommApp {
     @Override
     public void createMeeting(String titre, Calendar dateDebut, Calendar dateFin, String participant) {
         JSONMessage jsMessage = new JSONMessage();
-
         try {
             jsMessage.setField(JSONMessage.Fields.SENDER, "LOCAL");
             jsMessage.setField(JSONMessage.Fields.ADDRESSEES, participant);
             jsMessage.setField(JSONMessage.Fields.COMMAND, "JeSAISpAS"); // TODO je ne sais pas le nom de la commande
+            // TODO : ajouter à la message queue de l'agent
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -27,17 +27,38 @@ public class CommAppImpl implements InterfaceCommApp {
 
     @Override
     public void deleteMeeting() {
-
+        JSONMessage jsMessage = new JSONMessage();
+        try {
+            jsMessage.setField(JSONMessage.Fields.SENDER, "LOCAL");
+            jsMessage.setField(JSONMessage.Fields.COMMAND, "JeSAISpAS"); // TODO je ne sais pas le nom de la commande
+            // TODO : ajouter à la message queue de l'agent
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void modifyMeeting() {
-
+        JSONMessage jsMessage = new JSONMessage();
+        try {
+            jsMessage.setField(JSONMessage.Fields.SENDER, "LOCAL");
+            jsMessage.setField(JSONMessage.Fields.COMMAND, "JeSAISpAS"); // TODO je ne sais pas le nom de la commande
+            // TODO : ajouter à la message queue de l'agent
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void availability() {
-
+        JSONMessage jsMessage = new JSONMessage();
+        try {
+            jsMessage.setField(JSONMessage.Fields.SENDER, "LOCAL");
+            jsMessage.setField(JSONMessage.Fields.COMMAND, "JeSAISpAS"); // TODO je ne sais pas le nom de la commande
+            // TODO : ajouter à la message queue de l'agent
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -52,7 +73,7 @@ public class CommAppImpl implements InterfaceCommApp {
             request.setField(JSONMessage.Fields.ACTIVITY, "POSTIP");
             request.setField(JSONMessage.Fields.ID, id);
             request.setField(JSONMessage.Fields.PASSWORD, password);
-            // TODO : send to message QUEUE
+            // TODO : ajouter a la message queue du communicant externe
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -63,7 +84,7 @@ public class CommAppImpl implements InterfaceCommApp {
         JSONMessage request = new JSONMessage();
         try {
             request.setField(JSONMessage.Fields.ACTIVITY, "UPDATETABLE");
-            // TODO : send to message QUEUE
+            // TODO : ajouter a la message queue du communicant externe
         } catch (JSONException e) {
             e.printStackTrace();
         }
